d76a950f7336c710c5053d62efd88f97c1394d95
dan-peveto <daniel_peveto@outlook.com>
2023-12-04 13:34:20 -0800

    Create PuzzleGame class and ctor

---
+++ PuzzleGame.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JFrame;
+
+public class PuzzleGame extends JFrame{
+
+    public PuzzleGame() {
+        super("Puzzle Game");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
+    }
+}
