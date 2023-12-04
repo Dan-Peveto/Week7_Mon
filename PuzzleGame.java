025f40c0009c938026c97fbc5c439fe55ca4adea
dan-peveto <daniel_peveto@outlook.com>
2023-12-04 14:36:21 -0800

    added BufferedImage Method

--- PuzzleGame.java
+++ PuzzleGame.java
@@ -1,6 +1,10 @@
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PuzzleGame extends JFrame{
@@ -18,6 +22,11 @@ public class PuzzleGame extends JFrame{
         panel = new JPanel();
         panel.setLayout(new GridLayout(ROWS, COLUMNS)); // have to manually import java.awt.*; 
         add(panel);
+        try {
+            loadImage();
+        } catch(IOException e) {
+            JOptionPane.showMessageDialog(rootPane, "Error loading the image");
+        }
         buttonList = new ArrayList<FancyButton>();
         for(int i = 0; i < COLUMNS * ROWS; i++) {
             FancyButton btn = new FancyButton();
@@ -30,5 +39,9 @@ public class PuzzleGame extends JFrame{
         setResizable(false);
         setSize(width, height);
         setLocation(700, 0);
+    }
+
+    private BufferedImage loadImage() throws IOException {
+        return ImageIO.read(new File("Liverpool.jpg"));
     }
 }
